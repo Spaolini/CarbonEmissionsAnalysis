3b3fd713d6dbd004b2d79409d98eede57bff8c3f
Stephen Paolini <60363064+Spaolini@users.noreply.github.com>
2022-10-28 00:06:29 -0400

    Updated Code - Needs csv writing

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-import math
-import os
-import flask
-from flask import Flask, render_template, redirect, current_app, url_for, request
 import csv
 import json
+import math
+import pandas as pd
 import plotly
 import plotly.express as px
-import pandas as pd
+from csv import writer
 import sqlite3
+from flask import Flask, render_template, current_app
+from plotly.offline import plot
+import plotly.graph_objs as go
 
 app = Flask(__name__)
 
@@ -20,26 +21,27 @@ def landing():
 @app.route('/home.html')
 def home():  # put application's code here
     df = pd.read_csv("Data_Scaled.csv")
-    avgCO2 = math.floor(df['CO2_Emissions'].mean() * 100)
-    stdCO2 = df['CO2_Emissions'].std()
-    print(stdCO2)
+    de = pd.read_csv("highEmitter.csv")
+    df.head()
+    meanco2 = df['CO2_Emissions'].mean()
+    stdco2 = df['CO2_Emissions'].std()
+    above = meanco2 + (stdco2 * 2)
     CO2List = df.CO2_Emissions.tolist()
+    facList = df.Facility.tolist()
+    df['e'] = (df['CO2_Emissions'].mean() + (df['CO2_Emissions'].std()))
 
-    # avgCH4 = math.floor(df['CH4_Emissions'].mean() * 100)
-    # CH4LIst = df.CH4_Emissions.toList()
+    graph = px.bar(df, title="CO2 Emissions by Facility", x="Facility", y="CO2_Emissions", error_y="e")
+    chart = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
 
-    for i in range(len(CO2List)):
-        # CO2List[i] = math.floor(CO2List[i] * 100)
-        if CO2List[i] < stdCO2 * 3:
-            CO2List[i] = 'low emitter'
+    for i in range(len(df)):
+        if CO2List[i] > (df['CO2_Emissions'].mean() + (df['CO2_Emissions'].std() * 2)):
+            print(facList[i])
+            print(CO2List[i])
 
-        elif CO2List[i] > stdCO2 * 3:
-            CO2List[i] = 'high emitter'
-    print(CO2List)
+    graph2 = px.bar(df, title="CO2 High Emitters", x=facList, y=CO2List)
+    chart2 = json.dumps(graph2, cls=plotly.utils.PlotlyJSONEncoder)
 
-    graph = px.pie(df, title='High Emitters vs Low Emitters', values='CO2_Emissions', names="Category")
-    graphJSON = json.dumps(graph, cls=plotly.utils.PlotlyJSONEncoder)
-    return render_template("home.html", graphJSON=graphJSON)
+    return render_template("home.html", chart=chart, chart2=chart2)
 
 
 @app.route('/map.html')
@@ -49,5 +51,3 @@ def map_page():
 
 if __name__ == '__main__':
     app.run()
-
-# test branch
